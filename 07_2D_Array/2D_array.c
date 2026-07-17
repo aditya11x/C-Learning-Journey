6cd631350e933187bab20eec3f08a1b5e45ff400
aditya11x <kumaraditya225511@gmail.com>
2026-07-17 23:32:04 +0530

    Added a 2D Array Program

---
+++ 07_2D_Array/2D_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    int Number[3][3];
+    
+    printf("Enter 9 Numbers : ");
+    for( i = 0; i < 3; i++){
+        for( j = 0; j < 3; j++){
+            scanf("%d", &Number[i][j]);
+        }
+    }
+    printf("  \n");
+
+        for(i = 0; i < 3; i++){
+            for(j = 0; j < 3; j++){
+                printf("%d  ", Number[i][j]);
+            }
+            printf("\n");
+        }
+    
+    return 0;
+}
